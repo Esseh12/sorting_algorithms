1ae14d4b206bbf9ccc6ffdf5288765156cc32182
Doctware <yusclever9@gmail.com>
2024-02-20 11:17:38 +0100

    The header file

--- sort.h
+++ sort.h
@@ -10,13 +10,15 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
-const int n;
-struct listint_s *prev;
-struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
+/* Protottypes */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -24,4 +26,4 @@ void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-#endif
+#endif /* sort.h */
