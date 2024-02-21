807a864fded512c92d8a36d171443d400af2a91a
Doctware <yusclever9@gmail.com>
2024-02-21 01:57:01 +0100

    Implementing quick sort algorithim

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -36,8 +36,11 @@ int partition(int *array, int low, int heigh, size_t size)
 		if (array[j] < pivot)
 		{
 			i++;
-			swap(&array[i], &array[j]);
-			print_array(array, size);
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
 		}
 	}
 
